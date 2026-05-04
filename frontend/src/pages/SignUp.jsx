4921c312b188614a6d68215c52b0caa39b31b65b
Riley Herritt <rileyherritt06@gmail.com>
2026-05-04 16:48:34 -0400

    wip - need to make crop feature on file upload

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useState } from 'react';
+
 
 export const SignUp = () => {
+  
+  const crop = () => {
+
+  }
   return (
     <main className = "form-page">
         <div className="form-box">
@@ -9,6 +15,22 @@ export const SignUp = () => {
             <input type="text" id="newUsername" placeholder="Username" required/>
             <input type="password" id="newPassword" placeholder="Password" required/>
             <input type="password" id="confirmPassword" placeholder="Confirm Password" required/>
+
+            <fieldset>
+              <legend><strong>Set Up Your Profile</strong></legend>
+                <label htmlFor="uploadPfp">Profile Photo</label>
+                <input 
+                  id = "uploadPfp" 
+                  type = "file" 
+                  accept = "image/*"
+                  style = {{display: "none"}}
+                />
+                <button>Upload Photo</button>
+                
+                <label htmlFor='bio-box'>Add a Bio:</label>
+                <textarea id='bio-box' className = "comment-box"></textarea>
+
+            </fieldset>
 
             <button id="signupButton">Create Account</button>
         </div>
