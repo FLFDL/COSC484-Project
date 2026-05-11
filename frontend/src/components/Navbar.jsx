d91bb3fd2b8f83cba0e3844d5b619d93ca06bbeb
Temilola-Lawal-TU <tlawal3@students.towson.edu>
2026-05-11 03:20:50 -0400

    Added log out button for logged in users, logged out users no longer have profile button option

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,20 +1,52 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 const Navbar = () => {
 
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch("http://localhost:5001/api/auth/me",
+      {
+        credentials: "include"
+      }
+    )
+      .then(response => setIsLoggedIn(response.ok))
+      .catch(() => setIsLoggedIn(false))
+  }, []);
+
+  const handleLogout = async () => {
+    await fetch("http://localhost:5001/api/auth/logout", {
+      method: "POST",
+      credentials: "include"
+    });
+    setIsLoggedIn(false);
+    navigate('/login');
+  }
+
   return (
-          <nav className = "navbar">
-            <NavLink to = "/" id = "petstagram-logo">Petstagram</NavLink>
-            <ul>
-                  <li><NavLink to = "/" end>Home</NavLink></li>
-                  <li><NavLink to = "/about">About</NavLink></li>
-                  <li><NavLink to = "/community-guidelines">Community Guidelines</NavLink></li>
-                  <li><NavLink to = "/sign-up">Sign up</NavLink></li>
-                  <li><NavLink to = "/login">Login</NavLink></li>
-                  <li><NavLink to = "/profile">Profile</NavLink></li>
-            </ul>
-          </nav>
+    <nav className="navbar">
+      <NavLink to="/" id="petstagram-logo">Petstagram</NavLink>
+      <ul>
+        <li><NavLink to="/" end>Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/community-guidelines">Community Guidelines</NavLink></li>
+        {isLoggedIn ? (
+          <>
+            <li><NavLink to="/profile">Profile</NavLink></li>
+            <li><a
+              onClick={handleLogout} style={{ cursor: 'pointer' }}>Log Out</a></li>
+          </>
+        ) : (
+          <>
+            <li><NavLink to="/sign-up">Sign up</NavLink></li>
+            <li><NavLink to="/login">Login</NavLink></li>
+          </>
+        )}
+      </ul>
+    </nav>
   )
 }
 
